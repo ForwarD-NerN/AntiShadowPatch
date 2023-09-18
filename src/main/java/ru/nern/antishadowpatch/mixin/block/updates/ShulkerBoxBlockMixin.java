77e203825e4b39c93304947f354bc4d945d33d98
Rafe <mlun42941@gmail.com>
2023-09-18 21:47:36 +0500

    New CCE code

--- src/main/java/ru/nern/antishadowpatch/mixin/block/updates/ShulkerBoxBlockMixin.java
+++ src/main/java/ru/nern/antishadowpatch/mixin/block/updates/ShulkerBoxBlockMixin.java
@@ -1,8 +1,12 @@
 package ru.nern.antishadowpatch.mixin.block.updates;
 
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import net.minecraft.block.ShulkerBoxBlock;
 import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.block.entity.ShulkerBoxBlockEntity;
+import net.minecraft.inventory.Inventory;
+import net.minecraft.screen.ScreenHandler;
 import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -13,10 +17,13 @@ import ru.nern.antishadowpatch.AntiShadowPatch;
 @Mixin(ShulkerBoxBlock.class)
 public class ShulkerBoxBlockMixin
 {
-	//Technically not the same, but works as intended
-	@ModifyArg(method = "getComparatorOutput", at = @At(value = "INVOKE",
-			target = "Lnet/minecraft/screen/ScreenHandler;calculateComparatorOutput(Lnet/minecraft/block/entity/BlockEntity;)I"), index = 0)
-	private @Nullable BlockEntity antishadowpatch_bringBackCCE(@Nullable BlockEntity blockEntity) {
-		return AntiShadowPatch.config.blocks.bringBackCCESuppression ? (ShulkerBoxBlockEntity) blockEntity : blockEntity;
+	@WrapOperation(
+			method = "getComparatorOutput",
+			at = @At(value = "INVOKE", target = "Lnet/minecraft/screen/ScreenHandler;calculateComparatorOutput(Lnet/minecraft/block/entity/BlockEntity;)I")
+	)
+	private int antishadowpatch_bringBackCCESuppression(BlockEntity blockEntity, Operation<Integer> original)
+	{
+		return AntiShadowPatch.config.blocks.bringBackCCESuppression ?
+				ScreenHandler.calculateComparatorOutput((Inventory) blockEntity) : original.call(ScreenHandler.class, blockEntity);
 	}
 }
